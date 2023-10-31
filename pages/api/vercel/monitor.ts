afa09c421ed945486530f1b15cd1d53942295b62
J <hj912113850123@hotmail.com>
2023-10-31 15:39:17 +0800

    filter none 200 response

--- pages/api/vercel/monitor.ts
+++ pages/api/vercel/monitor.ts
@@ -8,7 +8,10 @@ export default async function handler(
   
   res.setHeader('x-vercel-verify', '94b09fc6d44433709aa821cdd6c6c1de22260723')
 
-  await sendMessage(5879750850, JSON.stringify(req.body, null, 2))
+  const data = req.body
+  if (data[0].statusCode !== 200) {
+    await sendMessage(5879750850, JSON.stringify(req.body, null, 2))
+  }
   
   return res.status(200).json({
     code: 0,
