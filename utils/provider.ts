033958cca9856a003785ca77ff423f1c92430b6a
J <hj912113850123@hotmail.com>
2023-11-07 16:52:57 +0800

    fix astar rpc

--- utils/provider.ts
+++ utils/provider.ts
@@ -5,7 +5,7 @@ import { TotalSupportedChainIDs, RPCKeys } from "@/types/chain"
 
 // const alchemyNetworksWl = [ChainIDs.Ethereum, ChainIDs.Polygon, ChainIDs.OP, TestnetChainIDs.Goerli, TestnetChainIDs["Polygon Mumbai"], TestnetChainIDs["Optimism Goerli Testnet"]]
 const quickNodeNetworksWl = [ChainIDs.BSC, TestnetChainIDs['BNB Smart Chain Testnet'], ChainIDs.Scroll, TestnetChainIDs['Scroll Sepolia Testnet']]
-const constantsNetworksWl = [ChainIDs.Astar, TestnetChainIDs['Shibuya Testnet']]
+const constantsNetworksWl = [TestnetChainIDs['Shibuya Testnet']]
 
 export const parseRPCKeys = (rpcKeys: string): Record<'alchemy' | 'quickNode', Record<TotalSupportedChainIDs, string[]>> => {
   const { alchemy, quickNode } = JSON.parse(rpcKeys) as RPCKeys
@@ -110,6 +110,7 @@ export const createProvider = (network: number) => {
       providers.set(network, getConstantsProvider(network))
     } else {
       // alcemy provider
+      console.log(getAlchemyProvider(network))
       providers.set(network, getAlchemyProvider(network))
     }
 
